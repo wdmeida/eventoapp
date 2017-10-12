ef0051da909d9a73ca8f5bda88cb16bda3c7960a
Wagner Almeida <wdmeida@gmail.com>
2017-10-12 19:08:36 -0300

    Adding participants in events and delete event.

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,11 +1,15 @@
 package com.eventoapp.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eventoapp.models.Convidado;
 import com.eventoapp.models.Evento;
@@ -26,8 +30,16 @@ public class EventoController {
 	}
 	
 	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
-	public String form(Evento evento) {
-		er.save(evento);
+	public String form(@Valid Evento evento,
+					   BindingResult result,
+					   RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique os campos!");	
+		} else {
+			er.save(evento);
+			attributes.addFlashAttribute("mensagem", "Evento cadastrado com sucesso!");
+		}
 		return "redirect:/cadastrarEvento";
 	}
 	
@@ -44,14 +56,34 @@ public class EventoController {
 		Evento evento = er.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
 		mv.addObject("evento", evento);
+		
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
+		
 		return mv;
 	}
 	
+	@RequestMapping("/deletar")
+	public String deletarEvento(long codigo) {
+		Evento evento = er.findByCodigo(codigo);
+		System.out.println(evento.getNome());
+		er.delete(evento);
+		return "redirect:/eventos";
+	}
+	
 	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
-	public String adicionaConvidadoEvento(@PathVariable("codigo") long codigo, Convidado convidado) {
+	public String adicionaConvidadoEvento(@PathVariable("codigo") long codigo, 
+										  @Valid Convidado convidado,
+										  BindingResult result,
+										  RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+			return "redirect:/{codigo}";
+		}
 		Evento evento = er.findByCodigo(codigo);
 		convidado.setEvento(evento);
 		cr.save(convidado);
+		attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
 		return "redirect:/{codigo}";
 	}
 }//class EventoController
